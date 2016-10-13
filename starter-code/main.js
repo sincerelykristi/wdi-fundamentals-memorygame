9c0594febb0d8a134f1915f60036191c9338dbc7
sincerelykristi <kristilmurphy@gmail.com>
2016-10-13 16:30:58 -0400

    added if else JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,9 @@ var cardOne = "queen"
 var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
+
+if (cardTwo === cardFour && cardThree) {
+	alert("Sorry, try again.");
+}	else if ( cardOne === cardTwo || cardThree === cardFour) {
+	alert("You found a match!")
+}
