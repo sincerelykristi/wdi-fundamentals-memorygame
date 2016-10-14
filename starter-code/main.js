ece9e4d974442eddecaea91008b963f111d81f35
sincerelykristi <kristilmurphy@gmail.com>
2016-10-14 14:27:47 -0400

    added flipping cards, but broke display

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,25 +1,47 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen"
-var cardTwo = "queen"
-var cardThree = "king"
-var cardFour = "king"
+var cards = ['queen','queen','king','king'];
 
-/*if (cardTwo === cardFour && cardThree) {
-	alert("Sorry, try again.");
-}	else if ( cardOne === cardTwo || cardThree === cardFour) {
-	alert("You found a match!")
-}*/
+var cardsInPlay = [];
+
+
 
 var board = document.getElementById('game-board');
 
-var createCards = function() {
-	for (var i= 0; i < 4; i++) {
+function createBoard() {
+	for (i=0;i<cards.length;i++) {
 		var cardElement = document.createElement('div');
 		cardElement.className = 'card';
-		board.appendChild(cardElement);
 
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click' isTwoCards);
+
+		board.appendChild(cardElement);
+		board.appendChild(cardElement);
 	}
 }
 
-createCards();
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+		console.log(this.getAttribute('data-card'));
+		if (this.getAttribute('data-card') === 'king') {
+			this.innerHTML = "<img src='playing-card-161489_960_720.png'>";
+		}	else {
+			this.innerHTML = "<img src='playing-card-161494_960_720'>";
+		}
+		if (cardsInPlay.length === 2) {
+			isMatch(cardsInPlay);
+			cardsInPlay = [];
+		}
+}
+
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+	}	else {
+		alert("Sorry, try again.");
+	}
+}
+
+createBoard();
+
