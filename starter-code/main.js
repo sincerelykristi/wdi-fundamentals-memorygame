f045797f2447d9a672f73fc3331f3e6c61c10fba
sincerelykristi <kristilmurphy@gmail.com>
2016-10-14 12:25:32 -0400

    added js for new cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,21 @@ var cardTwo = "queen"
 var cardThree = "king"
 var cardFour = "king"
 
-if (cardTwo === cardFour && cardThree) {
+/*if (cardTwo === cardFour && cardThree) {
 	alert("Sorry, try again.");
 }	else if ( cardOne === cardTwo || cardThree === cardFour) {
 	alert("You found a match!")
+}*/
+
+var board = document.getElementById('game-board');
+
+var createCards = function() {
+	for (var i= 0; i < 4; i++) {
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		board.appendChild(cardElement);
+
+	}
 }
+
+createCards();
